b9b1e45f48322c7aa34fccd35d79630c5799d1f4
Nina Niiranen <nina.niiranen@getdreams.com>
2020-12-11 13:54:36 +0100

    test: check that open follows redirect correctly

--- sdk/src/androidTest/java/com/getdreams/views/DreamsViewTest.kt
+++ sdk/src/androidTest/java/com/getdreams/views/DreamsViewTest.kt
@@ -7,17 +7,20 @@
 package com.getdreams.views
 
 import android.app.Activity
-import android.content.Context
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.getdreams.Dreams
 import com.getdreams.Location
-import org.junit.Test
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 import java.util.Locale
+import java.util.concurrent.TimeUnit
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
@@ -25,20 +28,41 @@ class DreamsViewTest {
 
     @get:Rule
     var activityRule = ActivityScenarioRule(Activity::class.java)
-    private lateinit var context: Context
+    private lateinit var server: MockWebServer
 
     @Before
     fun setup() {
-        // TODO setup mock web server
-        Dreams.setup("clientId", "https://getdreams.com")
+        server = MockWebServer()
+        server.start()
+        Dreams.setup("clientId", server.url("/").toString())
     }
 
     @Test
     fun open() {
+        server.enqueue(
+            MockResponse()
+                .setResponseCode(302)
+                .addHeader("Location", server.url("/test").toString())
+        )
+        server.enqueue(
+            MockResponse()
+                .setResponseCode(200)
+        )
         activityRule.scenario.onActivity {
             val dreamsView = DreamsView(it)
-            dreamsView.open("", Location.Dreams, Locale.CANADA_FRENCH)
+            dreamsView.open("id token", Location.Dreams, Locale.CANADA_FRENCH)
         }
+        val initPost = server.takeRequest(1, TimeUnit.SECONDS)!!
+        assertEquals("/", initPost.path)
+        assertEquals("POST", initPost.method)
+        assertEquals("application/json; utf-8", initPost.getHeader("Content-Type"))
+        assertEquals("application/json", initPost.getHeader("Accept"))
+        val expectedBody = """{"clientId":"clientId","idToken":"id token","locale":"fr_CA"}"""
+        assertEquals(expectedBody, initPost.body.readUtf8())
+
+        val urlLoad = server.takeRequest(1, TimeUnit.SECONDS)!!
+        assertEquals("/test", urlLoad.path)
+        assertEquals("GET", urlLoad.method)
     }
 
     @Test
